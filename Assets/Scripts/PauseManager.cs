d9aef6bd8507ecdeef5a2ca0d72a65fe6e887a07
Marlou Ruiz <marlou_ruiz@dlsu.edu.ph>
2024-07-25 18:50:48 +0800

    Cursor Tweak

--- Assets/Scripts/PauseManager.cs
+++ Assets/Scripts/PauseManager.cs
@@ -15,32 +15,33 @@ public class PauseManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Escape) && pause == false)
         {
             Pause();
-            pause = true;
         }
-        else if(Input.GetKeyDown(KeyCode.Escape) && pause == true)
+        else if (Input.GetKeyDown(KeyCode.Escape) && pause == true)
         {
             Continue();
-            pause = false;
         }
     }
-
 
     public void Pause()
     {
         pausePanel.SetActive(true);
         Time.timeScale = 0;
+        pause = true;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
     }
 
     public void Continue()
     {
         pausePanel.SetActive(false);
         Time.timeScale = 1;
+        pause = false;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     public void BackToMenu()
     {
         SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
-
     }
-
 }
